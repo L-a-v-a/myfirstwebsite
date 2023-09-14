1f2dd420669d3ba54904d76a85ebc11402d014f7
Olivia <chinazaoguelina99@gmail.com>
2023-09-14 07:22:22 -0700

    service section

--- src/Components/ServiceCard.jsx
+++ src/Components/ServiceCard.jsx
@@ -1,7 +1,7 @@
 const ServiceCard = ({imgURL, label, subtext }) => {
   return (
     <div className=" flex-1 sm:w-[350px] sm:min-w-[350px] w-full ronded-[2opx] shadow-3xl px-10 py-16">
-      <div></div>
+      <div>go</div>
     </div>
   )
 }
